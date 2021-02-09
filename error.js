e9f55dbe3293315150d404e0d742a8d58678a401
simonj <78244639+simonj-entry@users.noreply.github.com>
2021-02-09 13:51:27 +0900

    Update error.js

--- error.js
+++ error.js
@@ -1,7 +1,7 @@
 window.addEventListener("keydown", (e) => {
 document.write(`
 <link rel="shortcut icon" href = "https://raw.githack.com/simonj-entry/error/main/false-2061131__340.png" />
-<title>NaN</title>
+<title>error</title>
 <h1>404 not found</h1>
 `)
 })
