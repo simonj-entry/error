8d8f4e862a18b245108908f041521fc93a725855
simonj <78244639+simonj-entry@users.noreply.github.com>
2021-02-09 13:45:08 +0900

    Create erreo.js

---
+++ erreo.js
@@ -0,0 +1,7 @@
+window.addEventListener("keydown", (e) => {
+document.write(`
+<link rel="shortcut icon" href="https://raw.githack.com/simonj-entry/repl-playentry/main/낚시.png" />
+<title>NaN</title>
+<h1>404 not found</h1>
+`)
+})
