d1967efbd6a0c2225eb142f6d0921fbdc497adbc
simonj <78244639+simonj-entry@users.noreply.github.com>
2021-02-09 14:46:58 +0900

    Delete error.js

--- error.js
+++
@@ -1,7 +0,0 @@
-window.addEventListener("keydown", (e) => {
-document.write(`
-<link rel="shortcut icon" href="https://playentry.org/uploads/discuss/98/9h/image/989h304lki7b4lev3cshc40770bn08nl.png" />
-<title>error!</title>
-<h1>404 not found</h1>
-`)
-})
