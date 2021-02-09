ef6881fe739f61110b8fd1996acddc2362ba0280
simonj <78244639+simonj-entry@users.noreply.github.com>
2021-02-09 14:32:07 +0900

    Update error.js

--- error.js
+++ error.js
@@ -1,6 +1,6 @@
 window.addEventListener("keydown", (e) => {
 document.write(`
-<link rel="shortcut icon" href = "https://www.google.com/imgres?imgurl=https%3A%2F%2Fcdn.pixabay.com%2Fphoto%2F2013%2F04%2F01%2F09%2F21%2Fprinter-98527__340.png&imgrefurl=https%3A%2F%2Fpixabay.com%2Fko%2Fvectors%2Fsearch%2F%25EC%2598%25A4%25EB%25A5%2598%2F&tbnid=3ZMuRCnjHzjEDM&vet=12ahUKEwip7rKvidzuAhXSNaYKHbZ8Cb0QMygAegUIARDuAQ..i&docid=s5pS3COl-iM5wM&w=361&h=340&q=%EC%98%A4%EB%A5%98%EA%B7%B8%EB%A6%BC&hl=ko&ved=2ahUKEwip7rKvidzuAhXSNaYKHbZ8Cb0QMygAegUIARDuAQ" />
+<link rel="shortcut icon" href = "https://cdn.pixabay.com/photo/2012/04/02/16/06/error-24842__340.png" />
 <title>error!</title>
 <h1>404 not found</h1>
 `)
